ab9f4a1b3590e6c42a260019704ea852f3878842
TNthiagonet <thiago@zn11f.onmicrosoft.com>
2024-07-24 12:47:28 -0300

    ajustes e ajustes...3

--- src/puppeteerScript.js
+++ src/puppeteerScript.js
@@ -8,10 +8,8 @@ const { logger } = require('./src/utils/logger');
  */
 const addScriptTag = async (page, url) => {
   try {
-    // Aguardar o carregamento da página e a estabilidade
     await page.waitForSelector('body', { timeout: 30000 });
 
-    // Adicionar a tag do script
     await page.evaluate((scriptUrl) => {
       const script = document.createElement('script');
       script.src = scriptUrl;
@@ -22,7 +20,7 @@ const addScriptTag = async (page, url) => {
     logger.info(`Tag de script adicionada: ${url}`);
   } catch (error) {
     logger.error('Erro ao adicionar a tag do script:', error);
-    throw error; // Re-lançar o erro para tratamento adicional se necessário
+    throw error;
   }
 };
 
@@ -33,19 +31,15 @@ const addScriptTag = async (page, url) => {
 const startBrowserAndAddScript = async (scriptUrl) => {
   let browser;
   try {
-    // Iniciar o navegador com a opção --no-sandbox
     browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-    // Navegar para uma página específica ou criar uma página em branco
-    await page.goto('https://example.com'); // Substitua pela URL real se necessário
-    await page.waitForNavigation({ waitUntil: 'networkidle0' }); // Aguardar navegação e carregamento completo
-
-    // Adicionar a tag do script após a página estar completamente carregada
     await addScriptTag(page, scriptUrl);
   } catch (error) {
     logger.error('Erro ao iniciar o navegador ou adicionar o script:', error);
@@ -56,5 +50,4 @@ const startBrowserAndAddScript = async (scriptUrl) => {
   }
 };
 
-// Exemplo de uso - Substitua pela URL real do script quando você souber
 startBrowserAndAddScript('https://example.com/script.js');
